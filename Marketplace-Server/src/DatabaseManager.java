ad1231f102a07754f1d05e23bf13be1cfa892ea3
MuhammadMoataz <mohamedmoatazali60@gmail.com>
2022-06-27 23:21:32 +0200

    Added FindFile

--- Marketplace-Server/src/DatabaseManager.java
+++ Marketplace-Server/src/DatabaseManager.java
@@ -3,6 +3,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.io.*;
+import java.util.*;
 
 public class DatabaseManager {
     //to be modified
@@ -13,7 +15,14 @@ public class DatabaseManager {
     private static String ImagesDirectory;
 
     public static BufferedImage[] getImages(int ID) {
-        return null;
+        //convert to string
+        String s = String.valueOf(ID);
+
+        File file = new File("Marketplace-Server/database/images");
+        String[] arr = new String[10];
+        //arr -> array of paths of matched images
+        arr = findFile(s, file);
+
     }
 
     public static void start_connection() throws Exception {
@@ -21,5 +30,24 @@ public class DatabaseManager {
         PreparedStatement pstmt = null;
     }
 
+    public String[] findFile(String name,File file) {
+        File[] list = file.listFiles();
+        String[] arr = new String[10];
+        int str_indx = 0;
+        if (list != null)
+            for (File fil : list) {
+                boolean flag = true;
+                String s = fil.getName();
+                for (int i = 0; i < name.length(); i++) {
+                    if (name.charAt(i) != s.charAt(i))
+                        flag = false;
+                }
+                if (flag) {
+                    arr[str_indx++] = fil.getPath();
+                }
+
+            }
+        return arr;
+    }
     //    public static void store_item_data()
 }
