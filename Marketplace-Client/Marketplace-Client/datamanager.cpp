2dbb82e526ee30f40e8a842e8182b4fc1b2e9045
GhaadaAlaa <ghadaalaa2299@gmail.com>
2022-06-27 23:01:44 +0200

    Sign Up , server_response
    Done{
    * Check condition to return the signup result abort sign up if an invalid phone or email
    * Server Response in Slot
    * Read the Socket
    * Convert Text to
    JSON document
    * Convert to map to extract Qjson object members values
    * According to Response id emit the appropriate signal
    }
    Incomplete{
    * Hashing Password
    * Cases in Server Response
    }

--- Marketplace-Client/Marketplace-Client/datamanager.cpp
+++ Marketplace-Client/Marketplace-Client/datamanager.cpp
@@ -21,7 +21,7 @@ DataManager :: DataManager(unsigned int serverAddress, unsigned int serverPort){
 }
 bool validate_Email(const QString email)
 {
-bool result = true;
+    bool result = true;
     QRegularExpression regex("^[0-9a-zA-Z]+([0-9a-zA-Z][-._+])[0-9a-zA-Z]+@[0-9a-zA-Z]+([-.][0-9a-zA-Z]+)([0-9a-zA-Z][.])[a-zA-Z]{2,6}$");
     if(!regex.match(email).hasMatch())
     {
@@ -35,53 +35,92 @@ bool validate_Phone(const QString phone)
 {
 
     bool result = true;
-        QRegularExpression regex("+20(10|11|12|15)[0-9]{8}");
-        if(!regex.match(phone).hasMatch())
-        {
-            result = false;
-        }
-        return result;
+    QRegularExpression regex("+20(10|11|12|15)[0-9]{8}");
+    if(!regex.match(phone).hasMatch())
+    {
+        result = false;
+    }
+    return result;
 }
 
 
 
 SignUpResult  DataManager :: signUp  (SignUpData data){
-    SignUpResult result;
+    SignUpResult signUpResult;
 
-       if(validate_Email(data.email))
-       {
-          result.validEmail = true;
-       }
-       else
-       {
-           result.validEmail = false;
+    if(validate_Email(data.email))
+    {
+        signUpResult.validEmail = true;
+    }
+    else
+    {
+        signUpResult.validEmail = false;
 
-       }
+    }
 
-       if(validate_Phone(data.phone))
-       {
-          result.validPhone = true;
-       }
-       else
-       {
-           result.validPhone = false;
-         }
+    if(validate_Phone(data.phone))
+    {
+        signUpResult.validPhone = true;
+    }
+    else
+    {
+        signUpResult.validPhone = false;
+    }
+    if((signUpResult.validEmail == false)||(signUpResult.validPhone = false)){
 
-       QJsonObject signUpDataObject;
-       signUpDataObject.insert("RequestID", QJsonValue::fromVariant(SIGNUP_REQUEST));
-       signUpDataObject.insert("FirstName", QJsonValue::fromVariant(data.firstName));
-       signUpDataObject.insert("LastName", QJsonValue::fromVariant(data.lastName));
-       signUpDataObject.insert("Email", QJsonValue::fromVariant(data.email));
-       //hashing
-       //recordObject.insert("Password", QJsonValue::fromVariant(data.password));
-       signUpDataObject.insert("Address", QJsonValue::fromVariant(data.address));
-       signUpDataObject.insert("Phone", QJsonValue::fromVariant(data.phone));
+        return signUpResult;
+    }
 
-       QJsonDocument signUpJsonDoc(signUpDataObject);
-      //ToJson Compact
-       QByteArray signUpDataQByteArray = signUpJsonDoc.toJson(QJsonDocument::Compact);
-      //writeToSocket
-      socket.write(signUpDataQByteArray);
-      //connect Signals and Slots
-return result;
+    QJsonObject signUpDataObject;
+    signUpDataObject.insert("RequestID", QJsonValue::fromVariant(SIGNUP_REQUEST));
+    signUpDataObject.insert("FirstName", QJsonValue::fromVariant(data.firstName));
+    signUpDataObject.insert("LastName", QJsonValue::fromVariant(data.lastName));
+    signUpDataObject.insert("Email", QJsonValue::fromVariant(data.email));
+    //hashing
+    //recordObject.insert("Password", QJsonValue::fromVariant(data.password));
+    signUpDataObject.insert("Address", QJsonValue::fromVariant(data.address));
+    signUpDataObject.insert("Phone", QJsonValue::fromVariant(data.phone));
+
+    QJsonDocument signUpJsonDoc(signUpDataObject);
+    //ToJson Compact
+    QByteArray signUpDataQByteArray = signUpJsonDoc.toJson(QJsonDocument::Compact);
+    //writeToSocket
+    socket.write(signUpDataQByteArray);
+    //connect Signals and Slots
+
+    return signUpResult;
 }
+
+
+void DataManager :: server_response(){
+    //Read Socket
+    QByteArray serverResponse = socket.readAll();
+    //convert to JsonDocument
+    QJsonDocument serverJsonResponse = QJsonDocument::fromJson(serverResponse);
+    // Get QVarient
+    QVariant serverQvariant = serverJsonResponse.toVariant();
+    QMap serverQmap = serverQvariant.toMap();
+    //Extract ID
+    int serverResponseID = serverQmap["ID"].toInt();
+    bool serverbooleanResponse ;
+    switch (serverResponseID){
+    //ID for which Request
+    case SIGNUP_RESPONSE:
+        //Extract Result
+        serverbooleanResponse = serverQmap["Result"].toBool();
+        emit signUp_signal(serverbooleanResponse);
+        break;
+    case SIGNIN_RESPONSE:
+        //Extract Result
+        serverbooleanResponse = serverQmap["Result"].toBool();
+        emit signIn_signal(serverbooleanResponse);
+        break;
+    case WALLETDEPOSIT_RESPONSE:
+        //Extract Result
+        serverbooleanResponse = serverQmap["Result"].toBool();
+        emit walletDeposit_signal(serverbooleanResponse);
+        break;
+    }
+}
+
+
