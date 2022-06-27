41566feb51acc6b5844188b302279ddcae4bf32b
MuhammadMoataz <mohamedmoatazali60@gmail.com>
2022-06-27 21:07:33 +0200

    Start Connection

--- Marketplace-Server/src/DatabaseManager.java
+++ Marketplace-Server/src/DatabaseManager.java
@@ -1,9 +1,20 @@
 import java.awt.image.BufferedImage;
 public class DatabaseManager {
+    //to be modified
+    static final String DB_URL = "jdbc:mysql://localhost:3306/world";
+    static final String USER = "root";
+    static final String PASS = "Memo485213697";
 
     private static String ImagesDirectory;
 
     public static BufferedImage[] getImages(int ID) {
         return null;
     }
+
+    public static void start_connection() throws Exception {
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement stmt = connection.createStatement();
+    }
+
+    
 }
